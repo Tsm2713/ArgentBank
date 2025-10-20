acbfbf3e3d356fd54cdf630574b954faf094679b
Thomas CHE <jordanlafond76@gmail.com>
2025-10-20 23:32:11 +0200

    ajout fichier components

---
+++ frontend/src/components/Feature.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Feature({ icon, alt, title, description }) {
+  return (
+    <div className="feature-item">
+      <img src={icon} alt={alt} className="feature-icon" />
+      <h3 className="feature-item-title">{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default Feature;
